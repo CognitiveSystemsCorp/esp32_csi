769da8f9a1f0d0c31c5984e096020c06650d0dbc
Mikhail Zakharov <mikhail.zakharov@cognitivesystems.com>
2025-01-24 09:00:52 -0500

    initial

---
+++ README.md
@@ -0,0 +1,35 @@
+### Configure the project
+
+```
+git clone -b v5.4-beta1 https://github.com/espressif/esp-idf.git
+cd esp-idf
+./install.sh
+./export.sh
+cd ../
+```
+
+`idf.py --preview set-target esp32c5`
+
+Open the project configuration menu (`idf.py menuconfig`).
+
+In the `Project Configuration` menu:
+
+* Set the Wi-Fi configuration.
+    * Set `WiFi SSID`.
+    * Set `WiFi Password`.
+
+### Build and Flash
+
+Build the project and flash it to the board, then run the monitor tool to view the serial output:
+Run `idf.py -p PORT flash monitor` to build, flash and monitor the project.
+
+## Example Output
+```
+CSI_DATA,100,a0:36:bc:36:c7:38,-44,11,-92,11,12622117,14,0,128,1,"[0,0,0,0,0,0,0,0,0,0,2,-4,-8,39,-11,39,-14,38,-16,38,-18,37,-20,36,-21,34,-21,34,-22,32,-22,31,-21,31,-21,30,-20,30,-20,30,-18,29,-17,30,-15,29,-13,28,-13,28,-11,28,-10,26,-9,24,-7,23,-7,21,-6,20,-6,17,0,0,-7,12,-9,8,-10,6,-9,8,-10,6,-62,18,0,0,0,0,14,117,-24,-30,-110,1,0,0,64,76,-54,68,-91,-124,-72,19,0,0,0,0,23,117,-24,-30,-110,1,0,0,0,0,87,37,29,-58,-33,84,72,65,83,-23,96,-67,-119,-14,97,117]"
+CSI_DATA,101,a0:36:bc:36:c7:38,-43,11,-92,11,12720300,14,0,128,1,"[0,0,0,0,0,0,0,0,0,3,-1,0,-14,-31,-11,-33,-8,-34,-5,-35,-3,-36,-1,-36,1,-35,3,-35,5,-35,6,-34,6,-33,7,-33,8,-33,8,-33,7,-32,7,-32,6,-31,5,-30,6,-30,5,-29,5,-27,5,-26,4,-24,5,-22,5,-20,6,-18,0,0,8,-12,10,-9,11,-6,10,-9,11,-6,-7,-12,-8,-15,-7,-12,-8,-15,-15,-23,-17,-22,-18,-22,-18,-23,-18,-24,-19,-23,-18,-24,-19,-23,-11,-13,-10,-11,-8,-9,-8,-8,-7,-7,-3,-7,-2,-7,0,-8,1,-7,-6,10,18,1,0,0,0,0,0,0]"
+```
+
+## Visualize
+
+python `csi_data_parse_read.py -p PORT` can be used to visualize CSI data realtime
+
---
+++ csi_data_read_parse.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+
+# Copyright 2021 Espressif Systems (Shanghai) PTE LTD
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# WARNING: we don't check for Python build-time dependencies until
+# check_environment() function below. If possible, avoid importing
+# any external libraries here - put in external script, or import in
+# their specific function instead.
+
+import sys
+import csv
+import json
+import argparse
+import numpy as np
+
+import serial
+from os import path
+from io import StringIO
+
+from PyQt5.Qt import *
+from pyqtgraph import PlotWidget
+from PyQt5 import QtCore
+import pyqtgraph as pq
+
+import threading
+import time
+
+# Reduce displayed waveforms to avoid display freezes
+CSI_VAID_SUBCARRIER_INTERVAL = 1
+
+# Remove invalid subcarriers
+# secondary channel : below, HT, 40 MHz, non STBC, v, HT-LFT: 0~63, -64~-1, 384
+csi_vaid_subcarrier_color = []
+color_step = 255 // (28 // CSI_VAID_SUBCARRIER_INTERVAL + 1)
+
+LEGACY_LLTF_MASK = np.ones(64, dtype=bool)
+LEGACY_LLTF_MASK[:1] = False
+LEGACY_LLTF_MASK[27:27+10] = False
+LEGACY_LLTF_MASK[63:] = False
+
+
+LLTF_MASK = np.ones(64, dtype=bool)
+LLTF_MASK[:6] = False
+LLTF_MASK[32:33] = False
+LLTF_MASK[59:] = False
+
+# LLTF: 52
+csi_vaid_subcarrier_color += [(i * color_step, 0, 0) for i in range(1,  26 // CSI_VAID_SUBCARRIER_INTERVAL + 2)]
+csi_vaid_subcarrier_color += [(0, i * color_step, 0) for i in range(1,  26 // CSI_VAID_SUBCARRIER_INTERVAL + 2)]
+CSI_DATA_LLFT_COLUMNS = 26
+
+CSI_DATA_INDEX = 1  # buffer size
+CSI_DATA_COLUMNS = CSI_DATA_LLFT_COLUMNS
+DATA_COLUMNS_NAMES = ["type", "id", "mac", "rssi", "rate", "noise_floor", "channel", "local_timestamp", "sig_len", "rx_state", "len", "first_word", "data"]
+csi_data_array = np.zeros(53)
+
+class csi_data_graphical_window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.resize(1280, 720)
+        self.plotWidget_ted = PlotWidget(self)
+        self.plotWidget_ted.setGeometry(QtCore.QRect(0, 0, 1280, 720))
+
+        self.plotWidget_ted.setXRange(0, 53, padding=0)
+        self.plotWidget_ted.setYRange(0, 100)
+        #self.plotWidget_ted.addLegend()
+        self.plotWidget_ted.setBackground('w')
+        self.plotWidget_ted.setLabel('bottom', 'Carrier', units='')
+        self.plotWidget_ted.setLabel('left', 'Amplitude', units='')
+ 
+
+        self.curve_list = []
+
+        # print(f"csi_vaid_subcarrier_color, len: {len(csi_vaid_subcarrier_color)}, {csi_vaid_subcarrier_color}")
+
+        for i in range(CSI_DATA_INDEX):
+            curve = self.plotWidget_ted.plot(
+                csi_data_array, name=str(i), pen=csi_vaid_subcarrier_color[i])
+            self.curve_list.append(curve)
+
+        self.timer = pq.QtCore.QTimer()
+        self.timer.timeout.connect(self.update_data)
+        self.timer.start(100)
+
+    def update_data(self):
+        self.curve_list[0].setData(csi_data_array)
+
+    def closeEvent(self,event):
+        print('Closing')
+        self.subthread.running = False
+        #self.subthread.terminate()
+        self.subthread.wait()
+
+def csi_data_read_parse(self, port: str, mat_writer):
+    ser = serial.Serial(port=port, baudrate=115200,
+                        bytesize=8, parity='N', stopbits=1)
+    if ser.isOpen():
+        print("open success", mat_writer)
+    else:
+        print("open failed", mat_writer)
+        return
+    
+    self.running = True
+
+    csis = []
+    while self.running:
+        strings = str(ser.readline())
+        if not strings:
+            break
+        strings = strings.lstrip('b\'').rstrip('\\r\\n\'')
+        index = strings.find('CSI_DATA')
+
+        if index == -1:
+            # Save serial output other than CSI data
+            #log_file_fd.write(strings + '\n')
+            #log_file_fd.flush()
+            continue
+
+        csv_reader = csv.reader(StringIO(strings))
+        csi_data = next(csv_reader)
+
+        if len(csi_data) != len(DATA_COLUMNS_NAMES):
+            print("element number is not equal")
+            #log_file_fd.write("element number is not equal\n")
+            #log_file_fd.write(strings + '\n')
+            #log_file_fd.flush()
+            continue
+        
+        try:
+            csi_raw_data = json.loads(csi_data[-1])
+        except json.JSONDecodeError:
+            print("data is incomplete")
+            #log_file_fd.write("data is incomplete\n")
+            #log_file_fd.write(strings + '\n')
+            #log_file_fd.flush()
+            continue
+
+        # Reference on the length of CSI data and usable subcarriers
+        # https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-guides/wifi.html#wi-fi-channel-state-information
+        if len(csi_raw_data) != 128 and len(csi_raw_data) != 106 and len(csi_raw_data) != 384:
+            print(f"element number is not equal: {len(csi_raw_data)}")
+            #log_file_fd.write(f"element number is not equal: {len(csi_raw_data)}\n")
+            #log_file_fd.write(strings + '\n')
+            #log_file_fd.flush()
+            continue
+
+        #csv_writer.writerow(csi_data)
+
+        valid  = int(csi_data[-2])
+        x = np.array(csi_raw_data, dtype=np.float32)
+        z = x.reshape(-1, 2).view(np.complex64)
+        #print(csi_data[:-1])
+        print('valid', valid, 'rssi', int(csi_data[3]), 'ch', int(csi_data[6]), 'ts', int(csi_data[7]))
+
+        #if he:
+        #    x = np.squeeze(z[LLTF_MASK])
+        #else:
+        #    x = np.squeeze(z[LEGACY_LLTF_MASK])
+        #x = np.squeeze(z[LLTF_MASK])[:26]
+        x = np.squeeze(z)
+
+        #x = x[:26]
+        if mat_writer is not None:
+            csis.append(x)
+
+        y = np.abs(x)
+        y = y[:53]
+        csi_data_array[:len(y)] = y
+
+    ser.close()
+    if mat_writer is not None:
+        import scipy.io
+        scipy.io.savemat(mat_writer, dict(CSI=np.array(csis)))
+    return
+
+
+class SubThread (QThread):
+    def __init__(self, serial_port, save_file_name):
+        super().__init__()
+        self.serial_port = serial_port
+        self.save_file_name = save_file_name
+    def run(self):
+        csi_data_read_parse(self, self.serial_port, self.save_file_name)
+
+    def __del__(self):        
+        print('here')
+        #self.log_file_fd.close()
+
+
+if __name__ == '__main__':
+    if sys.version_info < (3, 6):
+        print(" Python version should >= 3.6")
+        exit()
+
+    parser = argparse.ArgumentParser(
+        description="Read CSI data from serial port and display it graphically")
+    parser.add_argument('-p', '--port', dest='port', action='store', required=True,
+                        help="Serial port number of csv_recv device")
+    parser.add_argument('-s', '--store', dest='store_file', action='store',
+                        help="Save the data printed by the serial port to a file")
+
+    args = parser.parse_args()
+    serial_port = args.port
+    file_name = args.store_file
+
+    app = QApplication(sys.argv)
+
+
+
+    window = csi_data_graphical_window()
+    window.subthread = SubThread(serial_port, file_name)
+    window.subthread.start()
+
+    window.show()
+
+    sys.exit(app.exec())
